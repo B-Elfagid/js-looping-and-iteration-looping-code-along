70b9ce09c0a7ec8619e4679fcbdcdbd512c6f542
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-04-25 12:11:06 +0100

    commit tree 2273df4376ad6e6cb3cd62892a5901c4fd91fa6e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+let num = 10
+function countDown(num){
+    while(num >= 0){
+        console.log(num)
+        num --
+    }
+}
+
+
